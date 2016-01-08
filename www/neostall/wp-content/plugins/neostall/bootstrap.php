4bd2db0f47aa4ced65e5ba8ee2bcc446f42ca255
mimoto.t <mimoto@w-tokyodo.com>
2016-01-08 19:18:45 +0900

    dev

--- www/neostall/wp-content/plugins/neostall/bootstrap.php
+++ www/neostall/wp-content/plugins/neostall/bootstrap.php
@@ -7,9 +7,15 @@ class Bootstrap {
 		$ins ?: $ins = new self();
 	}
 	private function __construct() {
-		Repository::parseJSON( file_get_contents( __DIR__ . '/models/repositories.json' ) );
-		ClassLoader::register( 'Neostall\\Model', __DIR__ . '/models' );
-		$yml = \Spyc::YAMLLoad( __DIR__ . '/entities/kitchencar.entity' );
+		#Repository::parseJSON( file_get_contents( __DIR__ . '/models/repositories.json' ) );
+		#ClassLoader::register( 'Neostall\\Model', __DIR__ . '/models' );
+		#$yml = \Spyc::YAMLLoad( __DIR__ . '/domain/_domain.yml' );
+		#var_dump( $yml );
+		$this->init_domains();
+	}
+	private function init_domains() {
+		$path = __DIR__ . '/domain/_domain.yml';
+		$yml  = yaml_parse_file( $path );
 		var_dump( $yml );
 	}
 }
